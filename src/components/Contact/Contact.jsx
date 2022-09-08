4bf15ad88b84cbe6f4f48d65f3e634cb722315d9
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-09-08 11:42:39 +0500

    Contact page done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Contact.scss";
+import logo from "../../assets/images/logo.jpg";
 
 function Contact() {
   return (
@@ -13,10 +14,30 @@ function Contact() {
         </p>
         <ul className='contact__list list-unstyled'>
           <li className="contact__item col-6">
-            slaom
+            <form action="/" className='contact__form'>
+              <div className='contact__input-holder'>
+                <input type="text" className='contact__input' placeholder='Name...' />
+              </div>
+              <div className='contact__input-holder'>
+                <input type="text" className='contact__input' placeholder='Surname...' />
+              </div>
+              <div className='contact__input-holder'>
+                <input type="tel" className='contact__input' placeholder='Tel...' />
+              </div>
+              <div className='contact__input-holder'>
+                <textarea className='contact__input' name="suggestions" id="suggestions" cols="30" rows="3" placeholder='Thoughts and suggestions' />
+              </div>
+              <button className='contact__btn'>
+                Send
+              </button>
+            </form>
+            <p className='text-center contact__info'>
+              <i class='bx bx-info-circle'></i>
+              Your data will not be given to third parties
+            </p>
           </li>
           <li className="contact__item col-6">
-            salom
+            <img src={logo} alt="Logo" />
           </li>
         </ul>
       </div>
